93f2c11e39daacb80eae6aa09aeed7c87e6255d3
Rishikesh Shinde <67017632+rdshinde@users.noreply.github.com>
2022-05-18 21:14:14 +0530

    feat: landing-page setup

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -1,4 +1,18 @@
 import styles from "./landing-page.module.css";
 export const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <>
+      <div className={styles.landing_page_wrapper}>
+        <section className={styles.hero_text_section}>
+          <div className={styles.logo_container}>
+            <h1>SayIt</h1>
+            <small>Just say it.</small>
+          </div>
+        </section>
+        <section className={styles.hero_img_section}>
+          <img src="assets/social-hero.svg" className="img-responsive" alt="hero" />
+        </section>
+      </div>
+    </>
+  );
 };
