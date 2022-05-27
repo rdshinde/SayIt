9e885536fdb199d7a3f416f31eb2b1a90bb36736
Rishikesh Shinde <67017632+rdshinde@users.noreply.github.com>
2022-05-27 18:42:29 +0530

    feat: aside done

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,30 @@
 import styles from "./sidebar.module.css";
 import { Sidenav } from "./sidenav/Sidenav";
-
+import Jdenticon from "react-jdenticon";
+import { AiOutlineLogout } from "../../services";
 export const Sidebar = () => {
   return (
-    <>
+    <aside className={styles.sidebar_container}>
       <Sidenav />
-    </>
+      <section className={styles.createpost_cta_container}>
+        <button className={`btn ${styles.createpost_cta}`}>
+          Create New Post
+        </button>
+      </section>
+      <section className={styles.user_info_container}>
+        <div className={styles.logout_wrapper}>
+          <div className={` m-x-md`}>
+            <Jdenticon size="48" />
+          </div>
+          <div>
+            <span className="text-4 bold-lg">Rishikesh Shinde</span>
+            <span className="text-secondary bold-md">@rdshinde</span>
+          </div>
+          <button className="btn text-3">
+            <AiOutlineLogout title="Logout" />
+          </button>
+        </div>
+      </section>
+    </aside>
   );
 };
--- src/services/react-icons.js
+++ src/services/react-icons.js
@@ -1,4 +1,4 @@
-export { AiFillHome } from "react-icons/ai";
+export { AiFillHome, AiOutlineLogout } from "react-icons/ai";
 export { IoMdRocket } from "react-icons/io";
 export { CgProfile } from "react-icons/cg";
 export { BsBookmarksFill, BsFillBellFill } from "react-icons/bs";
